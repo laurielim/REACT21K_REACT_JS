abdd4a7178a36176fea71e49078ee63070ecd5a7
laurielim <laurie.limsam@gmail.com>
2021-03-29 14:19:20 +0300

    create counter structures and event handlers

--- task-03-counter/src/App.js
+++ task-03-counter/src/App.js
@@ -1,7 +1,39 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  addHandler = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  removeHandler = () => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+  resetHandler = () => {
+    this.setState({ counter: 0 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="circle">{this.state.counter}</div>
+        <button onClick={this.addHandler} className="add">
+          Add one
+        </button>
+        <button onClick={this.removeHandler} className="remove">
+          Remove one
+        </button>
+        <button onClick={this.resetHandler} className="reset">
+          Reset
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
