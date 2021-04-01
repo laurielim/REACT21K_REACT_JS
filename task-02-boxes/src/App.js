2338f0c2b863b3c21fba108850182a0c435e7e4e
laurielim <laurie.limsam@gmail.com>
2021-04-01 11:08:00 +0300

    add react router

--- task-02-boxes/src/App.js
+++ task-02-boxes/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Main from "./Main";
+import Main_router from "./Main_router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Main />
+      <Main_router />
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ task-02-boxes/src/Main_router.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+    </div>
+  );
+};
+const Gallery = () => {
+  return (
+    <div>
+      <h1>This is the gallery page</h1>
+    </div>
+  );
+};
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const Main_router = () => {
+  return (
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/gallery" component={Gallery} />
+      </Switch>
+    </div>
+  );
+};
+
+export default Main_router;
