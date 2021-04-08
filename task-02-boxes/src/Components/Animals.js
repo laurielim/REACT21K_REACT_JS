4bf994b50cf3ba94d13daaf09264190a6247b795
laurielim <laurie.limsam@gmail.com>
2021-04-08 13:14:29 +0300

    add clickHandler method

--- task-02-boxes/src/Components/Animals.js
+++ task-02-boxes/src/Components/Animals.js
@@ -6,8 +6,7 @@ const AnimalCard = (props) => {
     <div className="box animal-card">
       <h2>{props.name}</h2>
       <img src={props.img} alt={altText} />
-
-      <button>Click Me</button>
+      <button onClick={props.clickMe}>Click Me</button>
     </div>
   );
 };
@@ -15,22 +14,35 @@ const AnimalCard = (props) => {
 class Animals extends Component {
   state = {
     animals: [
-      { name: "Fox", img: "https://source.unsplash.com/xUUZcpQlqpM/1600x1000" },
       {
+        id: 1,
+        name: "Fox",
+        img: "https://source.unsplash.com/xUUZcpQlqpM/1600x1000",
+      },
+      {
+        id: 2,
         name: "Rabbit",
         img: "https://source.unsplash.com/um1BsyEVB5U/1600x1000",
       },
       {
+        id: 3,
         name: "Wolf",
         img: "https://source.unsplash.com/WFPWB7Vum1E/1600x1000",
       },
     ],
   };
 
+  clickHandler = (name) => alert(`What does the ${name} say?`);
+
   render() {
     const animalsList = this.state.animals.map((animal) => {
       return (
-        <AnimalCard key={animal.name} name={animal.name} img={animal.img} />
+        <AnimalCard
+          key={animal.id}
+          name={animal.name}
+          img={animal.img}
+          clickMe={() => this.clickHandler(animal.name)}
+        />
       );
     });
 
