a8dea723ba2d69f311d09fee72ff77904512f7f2
laurielim <laurie.limsam@gmail.com>
2021-04-12 14:23:03 +0300

    add conditional game over message

--- task-04-speed-game/src/Components/GameOverOverlay.js
+++ task-04-speed-game/src/Components/GameOverOverlay.js
@@ -5,6 +5,18 @@ const closeHandler = () => {
 };
 
 const GameOverOverlay = (props) => {
+  let endText;
+  switch (true) {
+    case props.finalScore < 10:
+      endText = "If you don't SPLASH, you won't evolve.";
+      break;
+    case props.finalScore > 9 && props.finalScore < 20:
+      endText = "HARDEN for now, things will get better.";
+      break;
+    case props.finalScore > 19:
+      endText = "Never let your EMBER burn out.";
+  }
+
   return (
     <div className="overlay">
       <div className="popup">
@@ -12,6 +24,7 @@ const GameOverOverlay = (props) => {
           X
         </button>
         <h2>Game Over</h2>
+        <p>{endText}</p>
         <p>Your final score is {props.finalScore}</p>
       </div>
     </div>
